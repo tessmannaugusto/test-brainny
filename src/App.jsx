e95212c8221155fc9b96f02df4b78d69fbd4b5e1
tessmannaugusto <atessmannyunes@gmail.com>
2021-01-20 08:52:12 -0300

    :tada: create project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css'
+
+
+const App = props => {
+    return(
+        <div>
+            <h1>Test</h1>
+        
+        </div>
+    )
+}
+
+export default App;
